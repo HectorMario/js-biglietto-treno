08f5f178899fa2c21ae2bc7b6ad4f80d6a24e5b7
HectorMario <hector.mario.peraz@gmail.com>
2023-03-08 15:07:33 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const prezzo = 0.21 * (prompt ("cuanti chilometri farai"));
-console.log(prezzo);
 
-const ageUser = prompt("cuanti anni hai?")
-
-finalPrezzo = ""
-
-if(ageUser < 18){
-    finalPrezzo = ((prezzo) - (prezzo * 0.2)).toFixed(2);
-} else if(ageUser>=65){
-    finalPrezzo = ((prezzo) - (prezzo * 0.4)).toFixed(2);
-} else{
-    finalPrezzo = prezzo.toFixed(2);
+if(isNaN(prezzo)){
+    mensage = "Si prega di mettere solo numeri"
+    document.getElementById("price").innerHTML = mensage;
+} else {
+    const ageUser = prompt("cuanti anni hai?")
+    
+    if(isNaN(ageUser)){
+        mensage = "Si prega di mettere solo numeri"
+        document.getElementById("price").innerHTML = mensage;
+    } else {
+        finalPrezzo = ""
+        mensage = ""
+        if(ageUser < 18){
+            finalPrezzo = ((prezzo) - (prezzo * 0.2)).toFixed(2);
+        } else if(ageUser>=65){
+            finalPrezzo = ((prezzo) - (prezzo * 0.4)).toFixed(2);
+        } else{
+            finalPrezzo = prezzo.toFixed(2);
+        }
+        
+        console.log(finalPrezzo)
+        document.getElementById("price").innerHTML = "Devi Pagare $" + finalPrezzo;
+    }
 }
 
 
-console.log(finalPrezzo)
-document.getElementById("price").innerHTML = "Devi Pagare $" + finalPrezzo;
+
+
+
+
